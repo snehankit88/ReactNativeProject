c23f14e514b07721f1ee18949c42ddeeb1decd25
snehankit chikhalekar <snehankit.c@sankeysolutions.com>
2020-11-30 19:59:14 +0530

    Hello world in React Native

---
+++ App.js
@@ -0,0 +1,34 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  View,
+  Text
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  return (
+
+<View>
+<Text>Hello World</Text>
+</View>
+
+  );
+};
+
+
+export default App;
